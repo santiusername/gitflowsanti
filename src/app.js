013d882205ab53b9fe2c7bb796c6dcc26377fb69
Florelia <frisbyfloresita@gmail.com>
2026-04-09 09:33:05 -0500

    se agrego la funcion suma al archivo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 export function saludar(nombre){
-return "hola" + nombre + ", bienvenido a aplicacion CodeRaider";
+return "hola " + nombre + ", bienvenido a aplicacion CodeRaider";
 }
 export function despedir(nombre){
 return "hasta pronto, " + nombre + "!CodeRider";
@@ -10,4 +10,7 @@ estado: "activo",
 version:"1.0.0",
 message: "sistema CodeRaider funcionando correctamente"
 };
+}
+export function sumar(a, b) {
+  return a + b;
 }
